161a76af42c1211ff5a26c22bc4c4ceba71d21a7
Ethan <ewer@wisc.edu>
2025-03-12 17:18:47 -0500

    faster greedy

--- reduced_attention_mapping/__init__.py
+++ reduced_attention_mapping/__init__.py
@@ -1,12 +1,17 @@
-from .linear_attention_mapping import LinearAttentionMapping, GreedyAttentionMapping, KLDivAttentionMapping, MSEAttentionMapping
+from .attention_mapping import AttentionMapping
 from .conv_mapping import ConvAttentionMapping
+from .linear_attention_mapping import (
+    AverageAttentionMapping,
+    GreedyAttentionMapping,
+    LinearAttentionMapping,
+)
 from .util import topk_overlap
 
 __all__ = [
+    "AttentionMapping",
     "LinearAttentionMapping",
     "GreedyAttentionMapping",
-    "MSEAttentionMapping",
-    "KLDivAttentionMapping",
-    "topk_overlap",
+    "AverageAttentionMapping",
     "ConvAttentionMapping",
+    "topk_overlap",
 ]
