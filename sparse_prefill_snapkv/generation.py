4fc5e48f892002ecd3e706d66787b6e394be8066
Ethan <ewer@wisc.edu>
2025-04-10 19:27:02 -0500

    ensure window_size % 64 == 0

--- sparse_prefill_snapkv/generation.py
+++ sparse_prefill_snapkv/generation.py
@@ -11,6 +11,14 @@ from .llama_util import update_llama_model_for_sparse_prefill_snapkv
 from .qwen2_util import update_qwen2_model_for_sparse_prefill_snapkv
 
 
+def ceil64(x: int) -> int:
+    return max(((x + 63) // 64) * 64, 64)
+
+
+def floor64(x: int) -> int:
+    return max((x // 64) * 64, 64)
+
+
 def sparse_prefill_snapkv_generate(
     model: LlamaForCausalLM | Qwen2ForCausalLM,
     input_ids: Tensor,
@@ -19,6 +27,7 @@ def sparse_prefill_snapkv_generate(
     max_capacity_prompt: int,
     kernel_size: int = 5,
     generation_kwargs: dict[str, Any] = {},
+    floor_window_size: bool = True,
 ) -> Tensor:
     if isinstance(model, LlamaForCausalLM):
         update_llama_model_for_sparse_prefill_snapkv(model)
@@ -28,7 +37,9 @@ def sparse_prefill_snapkv_generate(
         raise NotImplementedError()
 
     model.config.max_capacity_prompt = max_capacity_prompt
-    model.config.window_size = window_size
+    model.config.window_size = (
+        floor64(window_size) if floor_window_size else ceil64(window_size)
+    )
     model.config.kernel_size = kernel_size
 
     return model.generate(  # type: ignore
@@ -48,6 +59,7 @@ def lookahead_sparse_prefill_snapkv_generate(
     max_capacity_prompt: int,
     kernel_size: int = 5,
     generation_kwargs: dict[str, Any] = {},
+    floor_window_size: bool = True,
 ) -> Tensor:
     if isinstance(model, LlamaForCausalLM):
         update_llama_model_for_sparse_prefill_snapkv(model)
@@ -59,7 +71,11 @@ def lookahead_sparse_prefill_snapkv_generate(
     lookahead_size = lookahead_ids.shape[1] - input_ids.shape[1]
 
     model.config.max_capacity_prompt = max_capacity_prompt + lookahead_size + 1
-    model.config.window_size = window_size + lookahead_size + 1
+    model.config.window_size = (
+        floor64(window_size + lookahead_size + 1)
+        if floor_window_size
+        else ceil64(window_size + lookahead_size + 1)
+    )
     model.config.kernel_size = kernel_size
 
     extended_attention_mask = F.pad(attention_mask, (0, lookahead_size), value=1)
